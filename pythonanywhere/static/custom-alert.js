d8fcdcac289f9b8093197d13c3ff8c217088441c
Nathan <nathanvcappellen@solcon.nl>
2025-10-17 16:31:49 +0200

    group modal

--- pythonanywhere/static/custom-alert.js
+++ pythonanywhere/static/custom-alert.js
@@ -141,7 +141,7 @@
         const wrapper = document.createElement("div");
         Object.assign(wrapper.style, {
             position: "fixed",
-            zIndex: "9999",
+            zIndex: "10010",
             boxShadow: "0 4px 6px rgba(0,0,0,0.1)",
             borderRadius: "6px",
             overflow: "hidden",
